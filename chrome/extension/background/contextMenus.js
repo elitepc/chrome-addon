020e1c417016c412f4f278f08379d8a0d7e4da20
Fábio Santos <fabio.santos@chimeraprime.com>
2020-02-21 10:02:31 +0100

    feat: add context menu to teamlyzer and itjobs

--- chrome/extension/background/contextMenus.js
+++ chrome/extension/background/contextMenus.js
@@ -1,41 +1,58 @@
-let windowId = 0;
-const CONTEXT_MENU_ID = 'example_context_menu';
+import { teamlyzerUrl } from '../../../config';
 
-function closeIfExist() {
-  if (windowId > 0) {
-    chrome.windows.remove(windowId);
-    windowId = chrome.windows.WINDOW_ID_NONE;
-  }
-}
+const URL_PATTERNS = [
+  `${teamlyzerUrl}/*`,
+  'https://www.itjobs.pt/*',
+];
+const CONTEXT_MENU_ID = 'TL_context';
+const CREATE_REVIEW_MENU_ID = 'TL_create_menu';
 
-function popWindow(type) {
-  closeIfExist();
-  const options = {
-    type: 'popup',
-    left: 100,
-    top: 100,
-    width: 800,
-    height: 475,
-  };
-  if (type === 'open') {
-    options.url = 'window.html';
-    chrome.windows.create(options, (win) => {
-      windowId = win.id;
-    });
-  }
+function openUrl(url) {
+  chrome.tabs.create({ url: `${teamlyzerUrl}${url}` });
 }
 
 chrome.contextMenus.create({
   id: CONTEXT_MENU_ID,
-  title: 'React Chrome Extension Example',
+  title: 'Teamlyzer',
   contexts: ['all'],
-  documentUrlPatterns: [
-    'https://github.com/*'
-  ]
+  documentUrlPatterns: URL_PATTERNS,
 });
-
-chrome.contextMenus.onClicked.addListener((event) => {
-  if (event.menuItemId === CONTEXT_MENU_ID) {
-    popWindow('open');
-  }
+chrome.contextMenus.create({
+  parentId: CONTEXT_MENU_ID,
+  id: CREATE_REVIEW_MENU_ID,
+  title: 'Criar nova review',
+  contexts: ['all'],
+  documentUrlPatterns: URL_PATTERNS,
+});
+chrome.contextMenus.create({
+  parentId: CREATE_REVIEW_MENU_ID,
+  id: 'tl_review_job',
+  title: 'Criar review de emprego',
+  contexts: ['all'],
+  documentUrlPatterns: URL_PATTERNS,
+  onclick: () => openUrl('/users/job-review/step1'),
+});
+chrome.contextMenus.create({
+  parentId: CREATE_REVIEW_MENU_ID,
+  id: 'tl_review_interview',
+  title: 'Criar review de entrevista',
+  contexts: ['all'],
+  documentUrlPatterns: URL_PATTERNS,
+  onclick: () => openUrl('/users/interview-review/step1'),
+});
+chrome.contextMenus.create({
+  parentId: CREATE_REVIEW_MENU_ID,
+  id: 'tl_review_salary',
+  title: 'Criar review de salário',
+  contexts: ['all'],
+  documentUrlPatterns: URL_PATTERNS,
+  onclick: () => openUrl('/users/salary-review'),
+});
+chrome.contextMenus.create({
+  parentId: CONTEXT_MENU_ID,
+  id: 'tl_create_company',
+  title: 'Criar nova empresa',
+  contexts: ['all'],
+  documentUrlPatterns: URL_PATTERNS,
+  onclick: () => openUrl('/companies/new-company'),
 });
