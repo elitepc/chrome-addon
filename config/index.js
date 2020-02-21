cf096c4d7073010768312393b2f6ce28b751874f
Fábio Santos <fabio.santos@chimeraprime.com>
2020-02-21 17:59:39 +0100

    feat: scrape itjobs company info

--- config/index.js
+++ config/index.js
@@ -1,2 +1,9 @@
 export const teamlyzerUrl = 'https://pt.teamlyzer.com';
 export const blogUrl = 'https://blog.teamlyzer.com/';
+
+export const itJobsPaths = [
+  '^https?:\/\/www\.itjobs\.pt/empresa(.*)',
+  '^https?:\/\/www\.itjobs\.pt/oferta(.*)',
+];
+export const linkedInPaths = [];
+export const injectPaths = [...itJobsPaths, ...linkedInPaths];
